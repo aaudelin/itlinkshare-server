5c0babd40de38cf6578e13c87fb1900ad96cc854
Aurelien <aurelien.audelin@gmail.com>
2019-08-30 15:08:29 +0200

    [FIX] Remove blackbox usage

---
+++ src/authentication/AuthenticationKey.kt
@@ -0,0 +1,4 @@
+package fr.itlinkshare.server.authentication
+
+// TODO Change it and secure the file for production purposes
+const val SECRET = "6{~e9<}8+mR0eg`vw?~Hr4w(8[oe9-R9MA1N+wQ@w@N^]l1=Jk<MT1%GN~+p#3^"
